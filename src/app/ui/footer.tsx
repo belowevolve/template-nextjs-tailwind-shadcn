f42057427904d108f7db689851d310a65e50f025
belowevolve <130287859+belowevolve@users.noreply.github.com>
2024-06-20 23:01:28 +0300

    Init logo and typography

---
+++ src/app/ui/footer.tsx
@@ -0,0 +1,11 @@
+import { BelowEvolve } from "@/shared/ui/below-evolve";
+
+const Footer = () => {
+  return (
+    <footer className="fixed bottom-2 right-2 z-50">
+      <BelowEvolve />
+    </footer>
+  );
+};
+
+export { Footer };
